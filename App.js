c55133a66582b100271a3a17519e95f26a1149db
arvean1.labib@gmail.com <arvean1.labib@gmail.com>
2020-08-04 13:52:54 -0700

    Making this work

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 
 export default function App() {
+  console.log("App executed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Talk To Ted</Text>
+      <Button
+        title="Connect to bluetooth"
+        onPress={() => alert("Connecting")}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
